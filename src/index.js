4d87d9826946325e6f54f2c4c881c10508c26416
Andro Babu <rambabusaravanan@gmail.com>
2017-06-11 04:50:27 +0530

    remove unwanted from skeleton

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<p>Hello World</p>, document.getElementById('root'));
